b55ec5cf06f1882706e0f8845bbdf6ca9c4954f5
*andria <andrikirkitadze@gmail.com>
2024-02-29 06:24:37 +0400

    first commit

---
+++ Day 6/lesson6.py
@@ -0,0 +1,13 @@
+imput=print("what is your name")
+
+imput=print("what is your surname")
+
+imput=print("how old are you")
+
+imput=print("where do you live")
+
+imput=print("when were you born")
+
+imput=print("how tall are you")
+
+input=("what is your name + what is your surname + how old are you + where do you live + when were you born + how tall are you") # my name is andria, my surname is kirkitadze, i am 10 years, i live in kudadov street, 21 august 2013 years, i am 157 cm tall
